1173282cfe6e9a3935fae77ca19f434d9bb6dda1
chandlertee <chandler.thompson@gmail.com>
2017-08-02 17:42:49 -0500

    Redirect regardless of 404 response

--- django/contrib/redirects/middleware.py
+++ django/contrib/redirects/middleware.py
@@ -4,8 +4,9 @@ from django.conf import settings
 
 class RedirectFallbackMiddleware(object):
     def process_response(self, request, response):
-        if response.status_code != 404:
-            return response # No need to check for a redirect for non-404 responses.
+        # we want to redirect regardless of 404
+        """if response.status_code != 404:
+            return response # No need to check for a redirect for non-404 responses."""
         path = request.get_full_path()
         try:
             r = Redirect.objects.get(site__id__exact=settings.SITE_ID, old_path=path)
